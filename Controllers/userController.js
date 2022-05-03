31c2dd32163ff354493bd125a532b537b30d612a
zeusVikalp <100758326+zeusVikalp@users.noreply.github.com>
2022-05-04 00:40:47 +0530

    done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -20,7 +20,16 @@ async function SignUp(request,response, next){
 
 }
 
+async function signOut(req, res, next) {
+    //remove token from DB
+    const token = req.body.token;
+    await tokenModel.deleteOne({ token });
+    res.status(200).json({ status: "Success", message: "Token deleted successfully" });
+}
+
+
 
 module.exports = {
-    SignUp
+    SignUp,
+    signOut
 }
