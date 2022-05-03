6ac33bb4f24f55ae67cb2f6fd2b788cc7bb5e21c
Sushant Subhash Gaikwad <sushantgaikwad06@gmail.com>
2022-05-03 20:47:44 +0530

    Done

--- Routes/app.js
+++ Routes/app.js
@@ -18,15 +18,17 @@ app.use(express.json())
 // app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
 
 
-// app.use(
-//   cors({
-//     Host:"patreondatabase.herokuapp.com",
-//     Origin: "http://localhost:3000",
-//     methods: "GET,POST,PUT,DELETE",
- 
+app.use(
+  cors({
+    Host:"patreondatabase.herokuapp.com",
+    Origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+    "Access-Control-Allow-Creadentials" : true,
+    "Access-Control-Allow-Origin" : true
    
-//   })
-// );
+  })
+);
 
 
 app.get("/",(req,res)=>{
