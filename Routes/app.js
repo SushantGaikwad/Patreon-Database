31b2eb0125d35fa24585b6b5cba539601c64fa40
Sushant Gaikwad <sushantgaikwad06@gmail.com>
2022-05-06 04:16:59 +0530

    Profile Change

--- Routes/app.js
+++ Routes/app.js
@@ -14,7 +14,7 @@ const { body } = require("express-validator");
 const userModel = require('../Models/user.model');
 const app = express();
 
-const CLIENT_URL = "http://localhost:3000/";
+const CLIENT_URL = "http://localhost:3000/profile";
 app.use(express.json())
 
 // app.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));
